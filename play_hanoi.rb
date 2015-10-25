3d58b97d3f3bc48beebf2d66ddce192504a1286a
Sia <siakaramalegos@gmail.com>
2015-10-25 16:24:37 -0500

    Modifies move prompt instructions and creates next_action and move methods that convert user input to usable data

--- play_hanoi.rb
+++ play_hanoi.rb
@@ -16,14 +16,38 @@ class PlayHanoi
     puts @locations
   end
 
+  def quit
+    puts "Thank you for playing Tower of Hanoi!  Be sure to rate us in the App Store!"
+  end
+
+  def move user_move
+    # assumes valid move entered (TODO: validate input)
+    puts user_move.to_s
+    render
+    next_action
+  end
+
+  def next_action
+    puts "Enter move > "
+    action = gets.chomp
+
+    if action == 'q' || action == 'quit'
+      quit
+    else
+      user_move = action.split(',').map { |tower| tower.to_i }
+      move(user_move)
+    end
+  end
+
   def play
     puts "************************************"
     puts "***  Welcome to Tower of Hanoi!  ***"
     puts "************************************"
     puts "Instructions:"
-    puts "Enter where you'd like to move from and to in the format [1,3]. Enter 'q' to quit. Current Board:"
+    puts "Enter where you'd like to move from and to in the format using integers separated by a comma.  For example:\n Enter move > 1,3\nEnter 'q' to quit. \n\nCurrent Board:"
 
     render
+    next_action
   end
 end
 
